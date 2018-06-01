2f29aa830d4a123c5bb8a23562a645d719918db2
jtomori <tomori.juraj@gmail.com>
2018-06-01 22:30:23 +0200

    fixed getRenderNodes() function, now it works fine

--- scripts/python/bg_render.py
+++ scripts/python/bg_render.py
@@ -6,25 +6,26 @@ def getRenderNodes(node):
     """
     returns renderable nodes found in children of specified node
     """
-    render_nodes = ("rop_geometry", "geometry", "Redshift_ROP", "ifd", "arnold", "opengl", "baketexture::3.0", "rib", "ris", "ribarchive", "wren", "ifdarchive", "render", "rop_alembic", "brickmap", "merge", "channel", "comp", "dsmmerge", "fetch", "wedge", "shell", "null", "dop", "alembic", "filmboxfbx", "agent", "mdd")
 
-    node_type_name = node.type().name()
     node_children = node.allSubChildren()
-
     node_list = []
 
-    if node_type_name in render_nodes:
+    if isinstance(node, hou.RopNode):
         node_list.append(node)
         return node_list
     elif len(node_children) > 0:
         for n in node_children:
-            if n.type().name() in render_nodes:
+            if isinstance(n, hou.RopNode):
                 node_list.append(n)
     else:
         print("No render node was found.\n")
         return None
 
-    return node_list
+    if len(node_list) > 0:
+        return node_list
+    else:
+        print("No render node was found.\n")        
+        return None
 
 def bg_render(kwargs):
     """
@@ -40,9 +41,10 @@ def bg_render(kwargs):
     file_name = hou.hipFile.basename()
 
     for node in nodes:
-        top_node = node
-
         node_list = getRenderNodes(node)
+        if not node_list:
+            return
+
         if len(node_list) == 0:
             return
         elif len(node_list) == 1:
@@ -57,14 +59,13 @@ def bg_render(kwargs):
                 node = node_list[ selected[0] ]
 
         rop_path = node.path()
-        top_node_path = top_node.path()
 
         frame_by_frame = ""
         if kwargs["altclick"]:
             frame_by_frame = "I"
 
         hscript_cmd = "render -Va{0} {1}; quit".format(frame_by_frame, rop_path)
-        intro = "Rendering {0} in {1}".format(top_node_path, file_name)
+        intro = "Rendering {0} in {1}".format(rop_path, file_name)
         finish = "Rendering was finished, press [enter] to close terminal."
 
         bash_render_cmd = 'hbatch -c \\"{0}\\" {1}'.format(hscript_cmd, file_path)
